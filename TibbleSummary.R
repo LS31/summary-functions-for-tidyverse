3585e991969558d332a1b27e14189a9e5ecd2e04
LS <LS31@users.noreply.github.com>
2017-04-28 15:06:07 +0200

    Added summarise_na

--- TibbleSummary.R
+++ TibbleSummary.R
@@ -1,8 +1,8 @@
 library(tidyverse)
 
 #' Summarises continous data. For grouped tibbles, summary statistics are done
-# for each group seperately. By default, analyses all numeric variables. You
-# can select specific variables.
+#' for each group seperately. By default, analyses all numeric variables. You
+#' can select specific variables.
 #'
 #' @param x A tibble.
 #' @param ... Comma separated list of unquoted expressions. You can treat variable names like they are positions. Use positive values to select variables; use negative values to drop variables. (This uses dplyr::select.)
@@ -36,8 +36,46 @@ summarise_numeric <- function(x, ...) {
 #' @param freq_digits Number of digits to round percentages.
 #'
 #' @return A tibble with counts and frequencies.
-countfreq <- function(x, ..., sort = TRUE, freq_digits = 2) {
+countfreq <- function(x, ..., sort = FALSE, freq_digits = 1) {
   vars <- lazyeval::lazy_dots(...)
   count_(x, vars, sort = sort) %>%
     mutate("freq" = round(n / sum(n) * 100, freq_digits))
+}
+
+#' Counts missing observations (NA's) for all variables in a tibble, including
+#' (rounded) frequencies. By default, analyses all numeric variables. You
+#' can select specific variables.
+#'
+#' @param x A tibble.
+#' @param ... Comma separated list of unquoted expressions. You can treat variable names like they are positions. Use positive values to select variables; use negative values to drop variables. (This uses dplyr::select.)
+#' @param sort If TRUE will sort output in descending order of n.
+#' @param freq_digits Number of digits to round percentages.
+#'
+#' @return A tibble with counts and frequencies.
+summarise_na <- function(x,
+                         ...,
+                         sort = TRUE,
+                         freq_digits = 1,
+                         remove_nonmissing = TRUE) {
+  vars <- lazyeval::lazy_dots(...)
+  if (!is_empty(vars)) {
+    x <- select_(.data = x, .dots = vars)
+  }
+
+  n_obs <- nrow(x)
+
+  missingness <- summarise_all(x,
+                               funs(sum(is.na(.)))) %>%
+    gather(key = "variable", value = "n_missing") %>%
+    mutate("freq_missing" = round(n_missing / n_obs * 100, percentage_digits))
+
+  if (remove_nonmissing) {
+    missingness <- filter(missingness, n_missing > 0)
+  }
+
+  if (sort) {
+    missingness <- arrange(missingness, desc(n_missing))
+  }
+
+  missingness
 }
